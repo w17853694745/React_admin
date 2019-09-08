a72d25850e9f04a757a1127c0a0c51b468871041
didi <didi@qq.com>
2019-09-08 16:09:55 +0800

    over

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -2,6 +2,9 @@ import axios from "axios"
 import {message} from "antd"
 import qs from "qs"
 //标识需要发送ajax请求,生产环境才需要
+/* 
+注意:这段代码只有在解决跨域的时候才能使用,如果把内容放在服务器上,就不需要这段代码,不然会报错
+*/
 axios.defaults.baseURL=process.env.NODE_ENV==="production"?"/react_api":""
 axios.interceptors.request.use(
   config =>{
